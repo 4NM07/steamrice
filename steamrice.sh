23dcde02f1fdc2a63a48e224290deb704d473de7
4NM07 <anmol.katiyar24@spit.ac.in>
2025-10-06 21:36:48 +0530

    Renewed Script

--- steamrice.sh
+++ steamrice.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 #-------------------------------------------------------------------
 echo "Welcome to the steamrice automated arch install script"
 #------------------Check for bios or uefi---------------------------
@@ -17,7 +18,7 @@ inp=${inp,,}
 if [[ "${inp,,}" == "y" ]]; then 
 	echo "Enter the disk (e.g. sda or vda or nvme0n1):"
 	read disk0
-	disk0=/dev/$disk0
+	disk=/dev/$disk0
         mode="auto"
 	
 else
@@ -28,10 +29,14 @@ else
 	echo "Choose a home partition"
 	read disk2
 	disk2="/dev/$disk2"
+if [[ "$boot_mode" = "BIOS" ]]; then
+	echo "No BOOT partition needed"
+else
 	echo "Choose an "$boot_mode" partition"
 	read disk3
 	disk3="/dev/$disk3"
 	mode="manual"
+fi
 fi
 #----------------------FORMATTING DISKS--------------------------------
 echo "BE AWARE the disks/partitions you have chosen will be formatted all data on the disks will be wiped out"
@@ -46,35 +51,59 @@ fi
 if [[ "$mode" == "auto" ]]; then
 if [[ "$boot_mode" == "UEFI" ]]; then
   echo "DETECTED UEFI --> using GPT with ESP partition"
-	parted -s  "$disk0" mklabel gpt
-	parted -s  "$disk0" mkpart ESP fat32 1MiB 513MiB
-	parted -s "$disk0" set 1 esp on
-	parted -s "$disk0" mkpart primary ext4 513MiB 20GiB
-	parted -s "$disk0" mkpart primary ext4 20GiB 100%
-       disk3="${disk0}p1" #efi_partition
-	disk1="${disk0}p2" #root_partition
-	disk2="${disk0}p3" #home_partition 
+	parted -s  "$disk" mklabel gpt
+	parted -s  "$disk" mkpart ESP fat32 1MiB 513MiB
+	parted -s "$disk" set 1 esp on
+	parted -s "$disk" mkpart primary ext4 513MiB 20GiB
+	parted -s "$disk" mkpart primary ext4 20GiB 100%
+if [[ "$disk" =~ nvme ]]; then
+	suffix="p"
+else
+	suffix=""
+fi 
+      disk3="${disk}${suffix}1" #efi_partition
+	disk1="${disk}${suffix}2" #root_partition
+	disk2="${disk}${suffix}3" #home_partition 
 else 
     echo "DETECTED BIOS --> using MBR layout"
-    parted -s "$disk0" mklabel msdos 
-    parted -s "$disk0" mkpart primary ext4 1MiB 20GiB
-    parted -s "$disk0" mkpart primary ext4 20GiB 100% 
-    parted -s "$disk0" set 1 boot on
-	 disk3="${disk0}p1" #efi_partition
-        disk1="${disk0}p2" #root_partition
-        disk2="${disk0}p3" #home_partition
+    parted -s "$disk" mklabel msdos 
+    parted -s "$disk" mkpart primary ext4 1MiB 20GiB
+    parted -s "$disk" mkpart primary ext4 20GiB 100% 
+    parted -s "$disk" set 1 boot on
+if [[ "$disk" =~ nvme ]]; then
+        suffix="p"
+else
+        suffix=""
+fi  
+      disk1="${disk}${suffix}1" #root_partition
+        disk2="${disk}${suffix}2" #home_partition
 fi
 
+if [[ "$boot_mode" = "UEFI" ]]; then 
+	echo "Formatting disks have been succesfully completed. ROOT=$disk1, HOME=$disk2, $boot_mode=$disk3"
 else 
-	echo "Formatting chosen partitions....."
-	if [[ "$boot_mode" == "UEFI" ]]; then
-	mkfs.fat -F32 "$disk3"
+	echo "Formatting disk hace been succesfully completed. ROOT=$disk1, HOME=$disk2"
+fi
+#---------------Assigning Filesystems----------------------
+echo "Formatting chosen partitions....."
+        if [[ "$boot_mode" == "UEFI" ]]; then
+        mkfs.fat -F32 "$disk3"
 fi
  mkfs.ext4 "$disk1"
  mkfs.ext4 "$disk2"
-fi
-
-echo "Formatting disks have been succesfully completed. ROOT=$disk1, HOME=$disk2, $boot_mode=$disk3"
-
-
+#----------------------Mounting----------------------------
+mount /dev/$disk1 /mnt
+mount /dev/$disk2 /mnt/home
+[[ "$boot_mode" = "UEFI" ]] &&  mount --mkdir /dev/$disk3 /mnt/boot/efi
+#-------------Installing essential Packages--------------------
+pacstrap -K /mnt base linux linux-firmware
+#-------------using UUIDS-------------------------
+genfstab -U /mnt >> /mnt/etc/fstab
+----------------Fakerooting----------------------------------------
+arch-chroot /mnt /bin/bash -e <<EOF
+mkinitcpio -P
+echo "PLS set your root password this will be required to login"
+passwd 
+<<EOF
+------------------------------------------------------------------
 
